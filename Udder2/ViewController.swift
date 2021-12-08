dbdddafcfde4e9e98e3076ee7a7975bd0d2893f4
milanmach2 <milanmach2@gmail.com>
2021-12-08 15:42:31 +0100

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by Milan Mach on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
